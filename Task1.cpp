c75c1bbb8d85d71278513c083f38e9357feac259
WorldEditorCRO <112510957+dinorozac@users.noreply.github.com>
2024-02-28 21:10:07 -0800

    Create Task1.cpp
    Created an example of what adding your code into this repository will look like as well as any future endeavors.

---
+++ Task1.cpp
@@ -0,0 +1,12 @@
+/*
+*
+*   Title: [ADD TITLE HERE OF FILE]
+*   Author: [ADD NAME OF PERSON WORKING ON CODE]
+*   Collaborators: [IF NEED HELP FROM GROUP MEMBER WITH CODE, THE HELPER'S NAME GOES HERE, OTHERWISE LEAVE BLANK]
+*   Class: CPSC 323-03
+*   Date: [ADD DATE THAT BEGAN WORKING ON TASK]
+*   Purpose: Read data from an input file  
+*
+*/
+
+// Your code goes here
